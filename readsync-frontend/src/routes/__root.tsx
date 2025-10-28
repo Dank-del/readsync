42dfa6a27d3c0c56b77a7f897f1aed91df4556e7
Sayan Biswas <me@sayanbiswas.in>
2025-10-29 00:33:20 +0530

    fix: conditionally render TanStackDevtools in development mode

--- readsync-frontend/src/routes/__root.tsx
+++ readsync-frontend/src/routes/__root.tsx
@@ -14,18 +14,20 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
     <>
       <Outlet />
-      <TanStackDevtools
-        config={{
-          position: 'bottom-right',
-        }}
-        plugins={[
-          {
-            name: 'Tanstack Router',
-            render: <TanStackRouterDevtoolsPanel />,
-          },
-          TanStackQueryDevtools,
-        ]}
-      />
+      {import.meta.env.DEV && (
+        <TanStackDevtools
+          config={{
+            position: 'bottom-right',
+          }}
+          plugins={[
+            {
+              name: 'Tanstack Router',
+              render: <TanStackRouterDevtoolsPanel />,
+            },
+            TanStackQueryDevtools,
+          ]}
+        />
+      )}
     </>
   ),
 })
